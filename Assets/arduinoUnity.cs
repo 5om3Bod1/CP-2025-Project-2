12afc1eaf94ce4395f7dc5dbbd695b5686533a51
5om3Bod1 <brandonxiedesigner@gmail.com>
2025-03-03 10:41:22 -0500

    mar3.0

--- Assets/arduinoUnity.cs
+++ Assets/arduinoUnity.cs
@@ -1,18 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
+using System.Threading;
 
 public class arduinoUnity : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int messageInt;
+
+    Thread IOThread = new Thread(DataThread);
+    private static SerialPort sp;
+    private static string incomingMsg = "";
+
+    private static void DataThread()
     {
-        
+        // Mac - /dev/cu.usbmodem1101
+        // PC - COM
+        sp = new SerialPort("/dev/cu.usbmodem101", 9600);
+        sp.Open();
+
+        while (true)
+        {
+            incomingMsg = sp.ReadExisting();
+            Thread.Sleep(200);
+        }
     }
 
-    // Update is called once per frame
+    private void OnDestroy()
+    {
+        if (IOThread != null && IOThread.IsAlive)
+        {
+            IOThread.Abort();
+        }
+
+        if (sp != null && sp.IsOpen)
+        {
+            sp.Close();
+        }
+    }
+
+    private void Start()
+    {
+        IOThread.Start();
+    }
+
     void Update()
     {
-        
+        if (!string.IsNullOrEmpty(incomingMsg))
+        {
+            string trimmedMsg = incomingMsg.Trim();
+
+            if (int.TryParse(trimmedMsg, out messageInt))
+            {
+                Debug.Log(messageInt);
+
+                bool processed = false;
+
+                if (messageInt == 1)
+                {
+                    processed = true;
+                    //Call on a function, that includes Invoke("Off",1f)
+                }
+                else if (messageInt == 2)
+                {
+                    processed = true;
+                    //Call on a function, that includes Invoke("Off",1f)
+                }
+                else if (messageInt == 3)
+                {
+                    processed = true;
+                    //Call on a function, that includes Invoke("Off",1f)
+                }
+
+                if (processed)
+                {
+                    incomingMsg = "";
+                }
+            }
+            else
+            {
+                Debug.LogError("Failed to parse input: " + trimmedMsg);
+            }
+        }
     }
 }
